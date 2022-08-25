e32931c0b338a58033db033664125a686aae0ceb
BenChun <67034610+BenjaminChun@users.noreply.github.com>
2022-08-25 09:59:50 +0800

    update to footer and header (yet to be tested)

--- regex.js
+++ regex.js
@@ -4,92 +4,117 @@
 
 // -------global definitions------- 
 // DOMAIN
-let EN_DOMAIN_REGEX = new RegExp("en.creative.com","ig"); //i for case and g is for global search
-let UK_DOMAIN = "uk.creative.com";
-let GR_DOMAIN = "gr.creative.com";
-let NORDIC_DOMAIN = "nordic.creative.com";
-let ES_DOMAIN = "es.creative.com";
-let IT_DOMAIN = "it.creative.com";
-let DE_DOMAIN = "de.creative.com";
-let FR_DOMAIN = "fr.creative.com";
-let PL_DOMAIN = "pl.creative.com";
+const EN_DOMAIN_REGEX = new RegExp("en.creative.com","ig"); //i for case and g is for global search
+const UK_DOMAIN = "uk.creative.com";
+const GR_DOMAIN = "gr.creative.com";
+const NORDIC_DOMAIN = "nordic.creative.com";
+const ES_DOMAIN = "es.creative.com";
+const IT_DOMAIN = "it.creative.com";
+const DE_DOMAIN = "de.creative.com";
+const FR_DOMAIN = "fr.creative.com";
+const PL_DOMAIN = "pl.creative.com";
 // DOLLAR
-let EN_DOLLAR_REGEX = new RegExp("(&euro;|€)(\\w*)[.]?(\\w*)","g");
-let EN_TO_UK_DOLLAR = '&#163;$2.$3';
-let EN_TO_GR_DOLLAR = "$2,$3$1";
-let EN_TO_IT_DOLLAR = "$1$2,$3";
-let EN_TO_ES_DE_FR_DOLLAR = "$2,$3$1";
-let EN_TO_PL_DOLLAR = "$2$3,00zł";
+const EN_DOLLAR_REGEX = new RegExp("(&euro;|€)(\\w*)[.]?(\\w*)","g");
+const EN_TO_UK_DOLLAR = '&#163;$2.$3';
+const EN_TO_GR_DOLLAR = "$2,$3$1";
+const EN_TO_IT_DOLLAR = "$1$2,$3";
+const EN_TO_ES_DE_FR_DOLLAR = "$2,$3$1";
+const EN_TO_PL_DOLLAR = "$2$3,00zł";
 // FREE SHIPPING
-let EN_SHIP_REGEX = new RegExp("FREE SHIPPING","ig");
-let EN_TO_ES_SHIP = "ENV&Iacute;O GRATU&Iacute;TO";
-let EN_TO_IT_SHIP = "SPEDIZIONE GRATUITA";
-let EN_TO_DE_SHIP = "KOSTENLOSE LIEFERUNG";
-let EN_TO_FR_SHIP = "LIVRAISON GRATUITE";
-let EN_TO_PL_SHIP = "BEZPŁATNA DOSTAWA";
+const EN_SHIP_REGEX = new RegExp("FREE SHIPPING","ig");
+const EN_TO_ES_SHIP = "ENV&Iacute;O GRATU&Iacute;TO";
+const EN_TO_IT_SHIP = "SPEDIZIONE GRATUITA";
+const EN_TO_DE_SHIP = "KOSTENLOSE LIEFERUNG";
+const EN_TO_FR_SHIP = "LIVRAISON GRATUITE";
+const EN_TO_PL_SHIP = "BEZPŁATNA DOSTAWA";
 // SHOP NOW BUY NOW
-let EN_SHOP_NOW_BUY_NOW_REGEX = new RegExp("(SHOP NOW|BUY NOW)","ig");
-let EN_TO_ES_SHOP_NOW_BUY_NOW = "COMPRAR AHORA";
-let EN_TO_IT_SHOP_NOW_BUY_NOW = "ACQUISTA ORA";
+const EN_SHOP_NOW_BUY_NOW_REGEX = new RegExp("(SHOP NOW|BUY NOW)","ig");
+const EN_TO_ES_SHOP_NOW_BUY_NOW = "COMPRAR AHORA";
+const EN_TO_IT_SHOP_NOW_BUY_NOW = "ACQUISTA ORA";
 // SHOP NOW
-let EN_SHOP_NOW_REGEX = new RegExp("SHOP NOW","ig");
-let EN_TO_DE_SHOP_NOW = "JETZT EINKAUFEN"
-let EN_TO_FR_SHOP_NOW = "ACHETEZ MAINTENANT"
-let EN_TO_PL_SHOP_NOW = "KUPUJ TERAZ";
+const EN_SHOP_NOW_REGEX = new RegExp("SHOP NOW","ig");
+const EN_TO_DE_SHOP_NOW = "JETZT EINKAUFEN"
+const EN_TO_FR_SHOP_NOW = "ACHETEZ MAINTENANT"
+const EN_TO_PL_SHOP_NOW = "KUPUJ TERAZ";
 // BUY NOW
-let EN_BUY_NOW_REGEX = new RegExp("BUY NOW","ig");
-let EN_TO_DE_BUY_NOW = "JETZT KAUFEN";
-let EN_TO_FR_BUY_NOW = "ACHETER MAINTENANT";
-let EN_TO_PL_BUY_NOW = "KUP TERAZ";
+const EN_BUY_NOW_REGEX = new RegExp("BUY NOW","ig");
+const EN_TO_DE_BUY_NOW = "JETZT KAUFEN";
+const EN_TO_FR_BUY_NOW = "ACHETER MAINTENANT";
+const EN_TO_PL_BUY_NOW = "KUP TERAZ";
 // LEARN MORE
-let EN_LEARN_MORE_REGEX = new RegExp("LEARN MORE","ig");
-let EN_TO_ES_LEARN_MORE = "M&Aacute;S INFORMACI&Oacute;N"
-let EN_TO_IT_LEARN_MORE = "PER SAPERNE DI PI&Ugrave;"
-let EN_TO_DE_LEARN_MORE = "ERFAHREN SIE MEHR";
-let EN_TO_FR_LEARN_MORE = "EN SAVOIR PLUS";
-let EN_TO_PL_LEARN_MORE = "DOWIEDZ SIĘ WIĘCEJ";
+const EN_LEARN_MORE_REGEX = new RegExp("LEARN MORE","ig");
+const EN_TO_ES_LEARN_MORE = "M&Aacute;S INFORMACI&Oacute;N"
+const EN_TO_IT_LEARN_MORE = "PER SAPERNE DI PI&Ugrave;"
+const EN_TO_DE_LEARN_MORE = "ERFAHREN SIE MEHR";
+const EN_TO_FR_LEARN_MORE = "EN SAVOIR PLUS";
+const EN_TO_PL_LEARN_MORE = "DOWIEDZ SIĘ WIĘCEJ";
 // NEW
-let EN_NEW_REGEX = new RegExp("NEW\\b","ig");
-let EN_TO_ES_NEW = "NUEVO";
-let EN_TO_IT_NEW = "NUOVO";
-let EN_TO_DE_NEW = "NEU";
-let EN_TO_FR_NEW = "NOUVEAU";
-let EN_TO_PL_NEW = "NOWOŚĆ";
+const EN_NEW_REGEX = new RegExp("NEW\\b","ig");
+const EN_TO_ES_NEW = "NUEVO";
+const EN_TO_IT_NEW = "NUOVO";
+const EN_TO_DE_NEW = "NEU";
+const EN_TO_FR_NEW = "NOUVEAU";
+const EN_TO_PL_NEW = "NOWOŚĆ";
 // FREE WORTH
-let EN_FREE_WORTH_REGEX = new RegExp("FREE(.*?)worth","ig");
-let EN_TO_ES_FREE_WORTH = "GRATIS el$1valorado en";
-let EN_TO_IT_FREE_WORTH = "GRATIS il$1dal valore di";
-let EN_TO_DE_FREE_WORTH = "KOSTENLOSES$1im Wert von";
-let EN_TO_FR_FREE_WORTH = "GRATUIT$1d&rsquo;une valeur de";
-let EN_TO_PL_FREE_WORTH = "DARMOWY$1wart";
+const EN_FREE_WORTH_REGEX = new RegExp("FREE(.*?)worth","ig");
+const EN_TO_ES_FREE_WORTH = "GRATIS el$1valorado en";
+const EN_TO_IT_FREE_WORTH = "GRATIS il$1dal valore di";
+const EN_TO_DE_FREE_WORTH = "KOSTENLOSES$1im Wert von";
+const EN_TO_FR_FREE_WORTH = "GRATUIT$1d&rsquo;une valeur de";
+const EN_TO_PL_FREE_WORTH = "DARMOWY$1wart";
 // FREE
-let EN_FREE_REGEX = new RegExp("FREE\\b","ig");
-let EN_TO_ES_FREE = "GRATUITO";
-let EN_TO_IT_FREE = "GRATUITO";
-let EN_TO_DE_FREE = "KOSTENLOS";
-let EN_TO_FR_FREE = "GRATUIT";
-let EN_TO_PL_FREE = "BEZPŁATNE";
+const EN_FREE_REGEX = new RegExp("FREE\\b","ig");
+const EN_TO_ES_FREE = "GRATUITO";
+const EN_TO_IT_FREE = "GRATUITO";
+const EN_TO_DE_FREE = "KOSTENLOS";
+const EN_TO_FR_FREE = "GRATUIT";
+const EN_TO_PL_FREE = "BEZPŁATNE";
 // PROMO CODE
-let EN_PROMO_CODE_REGEX = new RegExp("(/help/)ordering(.*?)#i-have-a-promo-code-how-do-i-redeem","ig");
-let EN_TO_ES_PROMO_CODE =  "$1pedidos$2#tengo-un-c-digo-de-promoci-n-c-mo-lo";
-let EN_TO_IT_PROMO_CODE =  "$1ordine$2#ho-un-codice-promozionale-come-posso";
-let EN_TO_DE_PROMO_CODE = "$1bestellung$2#ich-habe-einen-aktionscode-wie-l-se-ich";
-let EN_TO_FR_PROMO_CODE =  "$1commande$2#j-39-ai-un-code-promo-comment-puis-je";
-let EN_TO_PL_PROMO_CODE =  "$1ordering$2#i-have-a-promo-code-how-do-i-redeem";
+const EN_PROMO_CODE_REGEX = new RegExp("(/help/)ordering(.*?)#i-have-a-promo-code-how-do-i-redeem","ig");
+const EN_TO_ES_PROMO_CODE =  "$1pedidos$2#tengo-un-c-digo-de-promoci-n-c-mo-lo";
+const EN_TO_IT_PROMO_CODE =  "$1ordine$2#ho-un-codice-promozionale-come-posso";
+const EN_TO_DE_PROMO_CODE = "$1bestellung$2#ich-habe-einen-aktionscode-wie-l-se-ich";
+const EN_TO_FR_PROMO_CODE =  "$1commande$2#j-39-ai-un-code-promo-comment-puis-je";
+const EN_TO_PL_PROMO_CODE =  "$1ordering$2#i-have-a-promo-code-how-do-i-redeem";
 // BROWSER VIEW
-let EN_BROWSER_VIEW_REGEX = new RegExp("((alt|title)(.*?))VIEW ALL DEALS\\s*>\\s*","ig");
-let EN_TO_FR_BROWSER_VIEW = "$1VOIR TOUTES LES OFFRES >";
-let EN_TO_IT_BROWSER_VIEW = "$1VISUALIZZA TUTTE LE OFFERTE>";
-let EN_TO_DE_BROWSER_VIEW = "$1ALLE ANGEBOTE ANZEIGEN>";
-let EN_TO_ES_BROWSER_VIEW = "$1VEA TODAS LAS OFERTAS >";
-let EN_TO_PL_BROWSER_VIEW = "$1ZOBACZ CAŁĄ OFERTĘ >";
+const EN_BROWSER_VIEW_REGEX = new RegExp("((alt|title)(.*?))VIEW ALL DEALS\\s*>\\s*","ig");
+const EN_TO_FR_BROWSER_VIEW = "$1VOIR TOUTES LES OFFRES >";
+const EN_TO_IT_BROWSER_VIEW = "$1VISUALIZZA TUTTE LE OFFERTE>";
+const EN_TO_DE_BROWSER_VIEW = "$1ALLE ANGEBOTE ANZEIGEN>";
+const EN_TO_ES_BROWSER_VIEW = "$1VEA TODAS LAS OFERTAS >";
+const EN_TO_PL_BROWSER_VIEW = "$1ZOBACZ CAŁĄ OFERTĘ >";
 // WHITE
-let EN_WHITE_REGEX = new RegExp("\\(WHITE\\)","ig");
-let EN_TO_FR_WHITE = "(BLANC)";
-let EN_TO_DE_WHITE = "(WEIß)";
-let EN_TO_IT_WHITE = "(BIANCO)";
-let EN_TO_ES_WHITE = "(BLANCO)";
-let EN_TO_PL_WHITE = "(BIAŁY)";
+const EN_WHITE_REGEX = new RegExp("\\(WHITE\\)","ig");
+const EN_TO_FR_WHITE = "(BLANC)";
+const EN_TO_DE_WHITE = "(WEIß)";
+const EN_TO_IT_WHITE = "(BIANCO)";
+const EN_TO_ES_WHITE = "(BLANCO)";
+const EN_TO_PL_WHITE = "(BIAŁY)";
+// HEADER
+// HEADER DISPLAY
+const EN_HEADER_DISPLAY_REGEX = new RegExp("If this email does not display correctly(.*)view its full version","ig");
+const EN_TO_IT_HEADER_DISPLAY = "Se non riesci a visualizzare correttamente questa email$1seleziona la versione completa";
+// HEADER FACEBOOK
+const EN_HEADER_FACEBOOK_REGEX = new RegExp("https://www.facebook.com/CreativeLabs","ig");
+const EN_TO_IT_HEADER_FACEBOOK = "https://www.facebook.com/creativeitalia";
+// HEADER INSTAGRAM
+const EN_IT_HEADER_INSTAGRAM_REGEX = new RegExp("https://instagram.com/CreativeLabs","ig");
+// FOOTER
+// FOOTER EXPLORE MORE
+const EN_FOOTER_EXPLORE_MORE_REGEX = new RegExp("EXPLORE MORE","ig");
+const EN_TO_IT_FOOTER_EXPLORE_MORE = "SCOPRI DI PIÙ SU";
+// FOOTER SPEAKERS
+const EN_FOOTER_SPEAKERS_REGEX = new RegExp("SPEAKERS(?=\b|<)","ig");
+const EN_TO_IT_FOOTER_SPEAKERS = "ALTOPARLANTI";
+// FOOTER HEADPHONES
+const EN_FOOTER_HEADPHONES_REGEX = new RegExp("HEADPHONES(?=<)(.*?\\|)","ig");
+const EN_TO_IT_FOOTER_HEADPHONES = "CUFFIE$1&nbsp;<br class='esd-mobile-hidden'>";
+// FOOTER WORK SOLUTIONS
+const EN_FOOTER_WORK_SOLUTIONS_REGEX = new RegExp("WORK SOLUTIONS(?=\b|<)","ig");
+const EN_TO_IT_FOOTER_WORK_SOLUTIONS = "SOLUZIONI LAVORATIVE";
+// FOOTER AUDIO ENTHUSIASTS
+const EN_FOOTER_AUDIO_ENTHUSIASTS_REGEX = new RegExp("AUDIO ENTHUSIASTS(?=\b|<)","ig");
+const EN_TO_IT_FOOTER_AUDIO_ENTHUSIASTS = "APPASSIONATI DI AUDIO";
 // EUR TO GBP
 function eur_to_gbp(text_input){
     return text_input.replace(/EUR 79/gi,"GBP 79");
@@ -183,6 +208,15 @@ function en_to_it(text_input) {
     result = result.replace(EN_BROWSER_VIEW_REGEX,EN_TO_IT_BROWSER_VIEW);
     //white
     result = result.replace(EN_WHITE_REGEX,EN_TO_IT_WHITE);
+    //header
+    result = result.replace(EN_HEADER_DISPLAY_REGEX,EN_TO_IT_HEADER_DISPLAY);
+    result = result.replace(EN_HEADER_FACEBOOK_REGEX,EN_TO_IT_HEADER_FACEBOOK);
+    //footer
+    result = result.replace(EN_FOOTER_EXPLORE_MORE_REGEX,EN_TO_IT_FOOTER_EXPLORE_MORE);
+    result = result.replace(EN_FOOTER_SPEAKERS_REGEX,EN_TO_IT_FOOTER_SPEAKERS);
+    result = result.replace(EN_FOOTER_HEADPHONES_REGEX,EN_TO_IT_FOOTER_HEADPHONES);
+    result = result.replace(EN_FOOTER_WORK_SOLUTIONS_REGEX,EN_TO_IT_FOOTER_WORK_SOLUTIONS);
+    result = result.replace(EN_FOOTER_AUDIO_ENTHUSIASTS_REGEX,EN_TO_IT_FOOTER_AUDIO_ENTHUSIASTS);
     return result;
 }
 // translate en to es
@@ -305,6 +339,17 @@ function manage_translate(){
         translation_btn.disabled = false;
     }
 }
+// copy text from output
+function copy_output() {
+    /* Get the text field */
+    const copyText = editor2.getValue();
+    console.log(copyText);
+     /* Copy the text inside the text field */
+    navigator.clipboard.writeText(copyText);
+  
+    /* Alert the copied text */
+    alert("Copied output text");
+}
 
 
 // --------run time code--------
@@ -321,4 +366,7 @@ btn.addEventListener('click', event => { //this is an anonymous function
     var text_output = translate(text_input,selection.value);
     populate(text_output);
 });
-
+let copy = document.getElementById("copy");
+copy.addEventListener('click',event => {
+    copy_output();
+});
